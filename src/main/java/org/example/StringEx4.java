e208a7c1f0b6307d7bffa0f0a05521a2f666fed7
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-24 19:47:28 +0200

    Ex4

---
+++ src/main/java/org/example/StringEx4.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class StringEx4 {
+    public static void main(String[] args) {
+        String string = "CAPS EQUALS SCREAMING";
+        System.out.println(string.toUpperCase());
+        System.out.println(string.toLowerCase());
+    }
+}
