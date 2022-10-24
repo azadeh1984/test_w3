4c6a2613c1a13403a0af90c4183a99dd9d6160f4
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-24 19:34:59 +0200

    Ex3

---
+++ src/main/java/org/example/StringEx3.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class StringEx3 {
+    public static void main(String[] args) {
+        String string = "Ok this is not as long!";
+        String rusult = string.substring(10);
+        System.out.println(rusult);
+    }
+}
