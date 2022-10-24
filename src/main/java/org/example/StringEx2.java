8f834b4df7bfb3de960a110052f55b742a4542d9
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-24 19:02:47 +0200

    Ex2

---
+++ src/main/java/org/example/StringEx2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class StringEx2 {
+    public static void main(String[] args) {
+        String string= "Even longer example sentence";
+        int index1 = string .indexOf("o");
+        System.out.println(index1);
+    }
+}
