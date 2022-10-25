23d3d1c4f3b4b2830775210ef5ac543e2524ae9c
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-25 19:16:11 +0200

    Ex5

---
+++ src/main/java/org/example/StringEx1b.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class StringEx1b {
+    public static void main(String[] args) {
+            String string= "Long example sentence";
+            int index1 = string .indexOf("6");
+            System.out.println(index1);
+        }
+    }
+
+
