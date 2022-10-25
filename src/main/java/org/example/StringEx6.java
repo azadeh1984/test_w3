0191bd94a217e9954bb2b0417844f4c49de1ccfb
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-25 19:45:19 +0200

    Ex6

---
+++ src/main/java/org/example/StringEx6.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class StringEx6 {
+    public static void main(String[] args) {
+        String str = "\tJ\ta\tv\ta\t ";
+        System.out.println(str.trim());
+
+    }
+}
---
+++ src/main/java/org/example/StringEx7.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StringEx7 {
+    public static void main(String[] args) {
+
+    }
+}
