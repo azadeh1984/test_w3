4ba74900d9f8f8f4dfb80cf81130cd27bf9e3478
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-25 19:15:18 +0200

    Ex5

---
+++ src/main/java/org/example/StringEx5.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class StringEx5 {
+    public static void main(String[] args) {
+    String text = "Java is the worst programming language!";
+        System.out.println(text.replace("worst", "best"));
+    }
+}
